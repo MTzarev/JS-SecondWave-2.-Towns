8dc2c440a407d56f298efb1f3968198b0a83fd7c
Miroslav Tsarev <mtzarev@gmail.com>
2024-09-07 20:45:21 +0300

    Update 2. Towns.js

--- 2. Towns.js
+++ 2. Towns.js
@@ -2,7 +2,6 @@ function towns(input) {
 
     for (let i = 0; i < input.length; i++) {
         let argument = input[i];
-
         let arr = [city, latitude, longitude] = argument.split(` | `);
         let formLat = Number(latitude).toFixed(2);
         let formLong = Number(longitude).toFixed(2);
@@ -10,8 +9,6 @@ function towns(input) {
 
         console.log(obj);
     }
-
-
 }
 towns(['Sofia | 42.696552 | 23.32601',
     'Beijing | 39.913818 | 116.363625']);
